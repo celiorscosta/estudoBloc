c77b5f135af9ea86dc8d0244c833b897893d0b9d
celiorscosta <celiorscosta@hotmail.com>
2020-12-24 12:47:59 -0300

    API.

--- lib/api.dart
+++ lib/api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'models/video.dart';
 
-const API_KEY = 'AIzaSyD9kOLY6bHcvkxORigM539V6pFogexRdbQ';
+const API_KEY = '';
 
 class Api {
   String _search;
